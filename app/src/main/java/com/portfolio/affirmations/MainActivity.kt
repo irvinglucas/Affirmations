8d3f4f5112250544fd98ecfd0cefe43e5e8f1057
Irving Lucas da Silva Melo <irving.lucas@gmail.com>
2022-09-26 19:26:11 -0300

    Added the recyclerview, all its structure and a lot of other things like theme, colors, styles etc.

--- app/src/main/java/com/portfolio/affirmations/MainActivity.kt
+++ app/src/main/java/com/portfolio/affirmations/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.portfolio.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.portfolio.affirmations.adapter.ItemAdapter
+import com.portfolio.affirmations.data.Datasource
+import com.portfolio.affirmations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // using ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = Datasource().loadAffirmations()
+
+//        val linearLayoutManager = LinearLayoutManager(this, HORIZONTAL, false)
+//        val gridLayoutManager = GridLayoutManager(this, 3)
+        binding.recyclerView.adapter = ItemAdapter(this, data)
+        binding.recyclerView.setHasFixedSize(true)
+//        binding.recyclerView.layoutManager = gridLayoutManager
     }
 }
---
+++ app/src/main/java/com/portfolio/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,41 @@
+package com.portfolio.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.portfolio.affirmations.R
+import com.portfolio.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.imageview)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
+        // creates a new ViewHolder when needed
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
